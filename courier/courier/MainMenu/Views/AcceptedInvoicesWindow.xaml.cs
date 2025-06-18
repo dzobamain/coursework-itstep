858807b2716dc50b79a93cd44ff7fd1861dd8d13
Ell-r <ell.herill.e@gmail.com>
2025-06-18 15:45:41 +0300

    Added AcceptedInvoicesWindow, MainMenuWindow & MainWindow in courier

---
+++ courier/courier/MainMenu/Views/AcceptedInvoicesWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿/*
+    AcceptedInvoicesWindow.xaml
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace courier.MainMenu.Views
+{
+    public partial class AcceptedInvoicesWindow : UserControl
+    {
+        private MainWindow _main;
+        public AcceptedInvoicesWindow(MainWindow main)
+        {
+            InitializeComponent();
+            _main = main;
+        }
+
+        private void IncomingInvoiceButton_Click(object sender, RoutedEventArgs e)
+        {
+            _main.ShowMainMenuWindow();
+        }
+    }
+}
---
+++ courier/courier/MainMenu/Views/MainMenuWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿/*
+    MainMenuWindow.xaml
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace courier.MainMenu.Views
+{
+    public partial class MainMenuWindow : UserControl
+    {
+        private MainWindow _main;
+        public MainMenuWindow(MainWindow main)
+        {
+            InitializeComponent();
+            _main = main;
+        }
+
+        private void AcceptedInvoiceButton_Click(object sender, RoutedEventArgs e)
+        {
+            _main.ShowAcceptedInvoicesWindow();
+        }
+    }
+}
