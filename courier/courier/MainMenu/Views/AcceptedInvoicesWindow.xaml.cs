84c9e0cb610330311fdbc31f070da30c919eac7d
dzobamain <knopkayt26@gmail.com>
2025-06-25 18:32:51 +0200

    Add display of accepted invoices in AcceptedInvoicesWindow
    - Loaded invoices from the accepted invoices JSON file (GetAcceptedInvoicePath)
    - Populated ObservableCollection<InvoiceDisplayItem> with these invoices
    - Bound the collection to the ListBox for displaying accepted invoices
    - Enabled showing detailed invoice info on item double-click

--- courier/courier/MainMenu/Views/AcceptedInvoicesWindow.xaml.cs
+++ courier/courier/MainMenu/Views/AcceptedInvoicesWindow.xaml.cs
@@ -1,43 +1,73 @@
 ﻿/*
-    AcceptedInvoicesWindow.xaml
- */
+    AcceptedInvoicesWindow.xaml.cs
+*/
 
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using courier;
 
 namespace courier.MainMenu.Views
 {
     public partial class AcceptedInvoicesWindow : UserControl
     {
         private MainWindow _main;
+        private ObservableCollection<InvoiceDisplayItem> invoices = new ObservableCollection<InvoiceDisplayItem>();
+        List<Invoice> listInvoice;
+
         public AcceptedInvoicesWindow(MainWindow main)
         {
             InitializeComponent();
             _main = main;
+
+            string path = new CourierDataPath().GetAcceptedInvoicePath();
+            string acceptedPath = new CourierDataPath().GetAcceptedInvoicePath();
+
+            JsonHandler jsonHandler = new JsonHandler();
+            listInvoice = jsonHandler.ReadInvoiceFromJson(path);
+
+            SetAllInvoiceDisplayItem(listInvoice);
+            allInvoicesListBox.ItemsSource = invoices;
         }
+
 
         private void IncomingInvoiceButton_Click(object sender, RoutedEventArgs e)
         {
             _main.ShowMainMenuWindow();
         }
 
-        private void AllInvoiceListBox_Click(object sender, RoutedEventArgs e)
+        private void AllInvoiceListBox_Click(object sender, MouseButtonEventArgs e)
         {
             if (allInvoicesListBox.SelectedIndex != -1)
             {
-                InformationAbouitInvoiceInMainMenuWindow infoWindow = new InformationAbouitInvoiceInMainMenuWindow();
-                infoWindow.ShowDialog();
+                InformationAboutInvoiceInAcceptedWindow informationAboutInvoiceInAcceptedWindow = new();
+
+                //if (!informationAboutInvoiceInAcceptedWindow.SetAllData(listInvoice[allInvoicesListBox.SelectedIndex]))
+                //{
+                //return;
+                //}
+
+                informationAboutInvoiceInAcceptedWindow.ShowDialog();
+            }
+        }
+
+        private void SetAllInvoiceDisplayItem(List<Invoice> allInvoices)
+        {
+            invoices.Clear();
+
+            foreach (var invoice in allInvoices)
+            {
+                invoices.Add(new InvoiceDisplayItem
+                {
+                    Description = string.IsNullOrWhiteSpace(invoice.ShipmentsDescription) ? "Без опису" : invoice.ShipmentsDescription,
+                    Progress = string.IsNullOrWhiteSpace(invoice.progress) ? "Невідомо" : invoice.progress,
+                    IsSelected = false,
+                    OriginalInvoice = invoice
+                });
             }
         }
     }
